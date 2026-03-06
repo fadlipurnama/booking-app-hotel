5b26290e9edaada14cf3d86163e8458ba418b7fa
Muhammad Fadli Purnama <110590829+fadlipurnama@users.noreply.github.com>
2026-03-06 22:50:32 +0700

    :sparkles: add static constants for navigation and app config

---
+++ src/constants/navigation.ts
@@ -0,0 +1,22 @@
+export const menuLinks = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Room", href: "/room" },
+  { name: "Contact", href: "/contact" },
+  { name: "My Reservation", href: "/my-reservation" },
+  { name: "Dashboard", href: "/admin/dashboard" },
+  { name: "Manage Room", href: "/admin/room" },
+];
+
+export const menuLinksFooter = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Room", href: "/room" },
+  { name: "Contact", href: "/contact" },
+];
+export const policyLinksFooter = [
+  { name: "Legal", href: "#" },
+  { name: "Term & Condition", href: "#" },
+  { name: "Payment Method", href: "/#" },
+  { name: "Privacy Policy", href: "/#" },
+];
